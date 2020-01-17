93449b98e27c485e72b8ec2329e6e5ad4eb1288a
watanase <veltread@gmail.som>
2020-01-17 20:31:45 +0900

    add front

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  root "posts#index"
 end
