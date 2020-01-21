8bf750a37987ff939765d4f5ccd4d415e78f14c7
watanase <veltread@gmail.som>
2020-01-21 20:40:00 +0900

    add follow not ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   root "posts#index"
-  resources :users, only: [:edit, :show] 
+  resources :users, only: [:edit, :show]
+  resources :relationships, only: [:create, :destroy]
   resources :posts do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
-    resources :favorites, only: [:create, :destroy]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_034207) do
+ActiveRecord::Schema.define(version: 2020_01_21_110018) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2020_01_21_034207) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -62,4 +72,6 @@ ActiveRecord::Schema.define(version: 2020_01_21_034207) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
