a737a102a0b5072acff78c4821f51246af34ba2d
watanase <veltread@gmail.som>
2020-03-15 17:57:18 +0900

    robocopによるファイルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,21 +11,21 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
   # コメント
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # いいね
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   # フォロー一覧
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy, inverse_of: :relationship
   has_many :followings, through: :active_relationships, source: :follower
-  #フォロワー一覧
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # フォロワー一覧
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: :relationship
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
 
-  validates :name,  length: { maximum: 10 }, presence: true
-  validates :profile,  length: { maximum: 100 }
+  validates :name, length: { maximum: 10 }, presence: true
+  validates :profile, length: { maximum: 100 }
 end
