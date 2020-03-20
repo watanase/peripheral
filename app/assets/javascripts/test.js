b774d90a6eff73e319232cfe195c8ff6e6a13f49
watanase <veltread@gmail.som>
2020-03-21 02:55:01 +0900

    function定義

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -25,16 +25,24 @@ $(function () {
                   </li>`;
     return html;
   }
+  let fileIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // 既に使われているindexを除外
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+  // $('.hidden-destroy').hide();
   // 
   // ファイルを選択したときの処理
   // 
+  function indexPlus() {
+    fileIndex.shift();
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+  }
   $('#image-box').on('change', '.js-file', function (e) {
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     const count = $('.js-remove').length;
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
@@ -42,10 +50,13 @@ $(function () {
       // if (hiddenCheck) hiddenCheck.attr('checked', false);
       $('#previews').append(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(targetIndex + 1));
-      // 入力済みのinput欄を非表示
+      // if (count < 3) $('#image-box').append(buildFileField(targetIndex + 1));
+      if (count < 3) {
+        $('#image-box').append(buildFileField(fileIndex[0]));
+        indexPlus()
+      }
       $(`#image_${targetIndex}`).hide();
-      if (count > 2) $(`#image_${targetIndex + 1}`).hide();
+
     }
   });
   // 
@@ -60,17 +71,20 @@ $(function () {
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().remove();
     $(`#image_${targetIndex}`).remove();
-    if (count == 4) $(`#image_4`).show();
-    for (var i = targetIndex; i < 4; i++) {
-      $(`#image_${i + 1}`).attr('data-index', `${i}`).attr('id', `image_${i}`);
-      $(`#post_images_attributes_${i + 1}_src`).attr(
-        'name', `post[images_attributes][${i}][src]`).attr(
-          'id', `post_images_attributes_${i}_src`);
-      $(`#label_${i + 1}`).attr(
-        'for', `post_images_attributes_${i}_src`).attr(
-          'id', `label_${i}`);
-      $(`#pre_${i + 1}`).attr('data-index', `${i}`).attr('id', `pre_${i}`);
-    };
+    if (count == 4) {
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      indexPlus()
+    }
+    // for (var i = targetIndex; i < 4; i++) {
+    //   $(`#image_${i + 1}`).attr('data-index', `${i}`).attr('id', `image_${i}`);
+    //   $(`#post_images_attributes_${i + 1}_src`).attr(
+    //     'name', `post[images_attributes][${i}][src]`).attr(
+    //       'id', `post_images_attributes_${i}_src`);
+    //   $(`#label_${i + 1}`).attr(
+    //     'for', `post_images_attributes_${i}_src`).attr(
+    //       'id', `label_${i}`);
+    //   $(`#pre_${i + 1}`).attr('data-index', `${i}`).attr('id', `pre_${i}`);
+    // };
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
@@ -80,7 +94,7 @@ $(function () {
   $(document).ready(function () {
     const remLength = $('.js-remove').length;
     const imageLength = $('.hidden-destroy').length
-    // if (imageLength > 0) $('.js-file_group').hide()
+    if (imageLength > 0) $('.js-file_group').hide()
     if (imageLength > 3) $('.js-file_edit').hide()
     if (remLength == 0) $("#image_1, #image_2, #image_3, #image_4").remove()
   });
