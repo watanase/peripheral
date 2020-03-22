d07d697a108eb9f4e534ca6c8a1cd8bd3ec10525
watanase <veltread@gmail.som>
2020-03-22 19:35:12 +0900

    ヘッダーをｊｓ処理

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,10 +2,10 @@
 // ユーザーネームマウスオーバー
 // 
 $(function () {
-  $('.nav-mypage').hover(function () {
-    $("ul:not(:animated)", this).slideDown();
+  $('.nav-mypage').click(function () {
+    $("ul:not(:animated)", this).slideDown(0);
   }, function () {
-    $("ul.drop-menu", this).slideUp();
+    $("ul.drop-menu", this).slideUp(0);
   });
 });
 // 
@@ -15,9 +15,22 @@ $(function () {
   $('.toggle').click(function () {
     $(this).toggleClass('active');
     if ($(this).hasClass('active')) {
-      $('.globalMenuSp').addClass('active');
+      $('.nav').addClass('active');
     } else {
-      $('.globalMenuSp').removeClass('active');
+      $('.nav').removeClass('active');
     }
   });
+});
+// 
+// ウィンドウのサイズで判断
+// 
+$(window).resize(function () {
+  var w = $(window).width();
+  var x = 900;
+  if (w > x) {
+    $('.nav').removeClass('active');
+  }
+  if (w < x) {
+    $('.toggle').removeClass('active');
+  }
 });
