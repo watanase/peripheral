4ca15f7db19d82f4d45e62b9399b6d996a4f4092
watanase <veltread@gmail.som>
2020-03-22 16:06:43 +0900

    ハンバーガーメニュー設置

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,7 +1,23 @@
+// 
+// ユーザーネームマウスオーバー
+// 
 $(function () {
   $('.nav-mypage').hover(function () {
     $("ul:not(:animated)", this).slideDown();
   }, function () {
     $("ul.drop-menu", this).slideUp();
+  });
+});
+// 
+// ハンバーガーメニューのクリック
+// 
+$(function () {
+  $('.toggle').click(function () {
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+      $('.globalMenuSp').addClass('active');
+    } else {
+      $('.globalMenuSp').removeClass('active');
+    }
   });
 });
