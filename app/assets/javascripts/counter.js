6f67506fa03d49a822041bbbaeaf7686f26cc323
watanase <veltread@gmail.som>
2020-03-25 02:36:42 +0900

    ｊｓ不具合削除

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -8,22 +8,4 @@ $(function () {
     let countNum = ($(this).val().length);
     $(".count-profile").html(countNum);
   });
-  // テキストエリアの自動調整
-  $(".input-text").height(30);//init
-  $(".input-text").css("lineHeight", "20px");//init
-
-  $(".input-text").on("input", function (evt) {
-    if (evt.target.scrollHeight > evt.target.offsetHeight) {
-      $(evt.target).height(evt.target.scrollHeight);
-    } else {
-      var lineHeight = Number($(evt.target).css("lineHeight").split("px")[0]);
-      while (true) {
-        $(evt.target).height($(evt.target).height() - lineHeight);
-        if (evt.target.scrollHeight > evt.target.offsetHeight) {
-          $(evt.target).height(evt.target.scrollHeight);
-          break;
-        }
-      }
-    }
-  });
 });
