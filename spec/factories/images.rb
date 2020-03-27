bb978847690ed9533e3f0956eae9b112e67b7ae8
watanase <veltread@gmail.som>
2020-03-27 14:41:09 +0900

    rspec修正

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :image do
-    src { File.open(Rails.root.join('app/assets/images/com.jpg')) }
+    src { File.open(Rails.root.join('app/assets/images/paper.jpg')) }
     association :post
   end
 end
