18ea9715fea91970e0155540864274a7dd66c828
watanase <veltread@gmail.com>
2020-05-04 16:01:07 +0900

    統合テスト/ログインから投稿

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,26 +4,24 @@ feature 'post', type: :feature do
   let(:user) { create(:user) }
 
   scenario 'ユーザー情報が更新されていること' do
-    # ログイン前には投稿ボタンがない
     visit root_path
-    expect(page).to have_no_content('投稿する')
+    expect(page).to have_no_content('新規投稿')
 
-    # ログイン処理
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    find('input[name="commit"]').click
+    click_on('ログインする')
     expect(current_path).to eq root_path
-    expect(page).to have_content('投稿する')
+    expect(page).to have_content('新規投稿')
 
-    # ポストの投稿
     expect {
-      click_link('投稿する')
+      click_link('新規投稿')
       expect(current_path).to eq new_post_path
-      fill_in 'image', with: 'https://s.eximg.jp/expub/feed/Papimami/2016/Papimami_83279/Papimami_83279_1.png'
+      find('input[type="file"]').click
+      attach_file "src", "#{Rails.root}/app/assets/images/paper.jpg"
       fill_in 'title', with: 'フィーチャスペックのテスト'
       fill_in 'content', with: 'フィーチャスペックのテスト'
       find('input[type="submit"]').click
-    }.to change(Tweet, :count).by(1)
+    }.to change(Post, :count).by(1)
   end
 end
